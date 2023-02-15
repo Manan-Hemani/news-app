ff1d5f0b87a31b56a853554c10b65257b9a7df2e
Manan <mananhemani1000@gmail.com>
2023-02-15 16:03:38 +0530

    created cards for displaying headlines

--- src/Components/News.js
+++ src/Components/News.js
@@ -1,9 +1,24 @@
 import React, { Component } from "react";
+import NewsItems from "./NewsItems";
 
 export default class News extends Component {
     render() {
         return (
-            <></>
+            <div className="container mt-2">
+                <h2>Headlines!</h2>
+                <div className="row">
+                    <div className="col-md-4">
+                        <NewsItems title="myTitle" description="WECJSDNKX" />
+                    </div>
+                    <div className="col-md-4">
+                        <NewsItems title="myTitle" description="WECJSDNKX" />
+                    </div>
+                    <div className="col-md-4">
+                        <NewsItems title="myTitle" description="WECJSDNKX" />
+                    </div>
+                </div>
+                
+            </div>
         )
     }
 }
--- src/Components/NewsItems.js
+++ src/Components/NewsItems.js
@@ -2,8 +2,18 @@ import React , { Component } from "react";
 
 export default class NewsItems extends Component {
     render() {
+        let {title,description} = this.props;
         return (
-            <></>
+            <>
+                <div className="card mt-3" style={{width:"18rem"}}>
+                    <img src="..." className="card-img-top" alt="..." />
+                        <div className="card-body">
+                            <h5 className="card-title">{title}</h5>
+                            <p className="card-text">{description}</p>
+                            <a href="/" className="btn btn-primary">Read More</a>
+                        </div>
+                </div>
+            </>
         )
     }
 }
