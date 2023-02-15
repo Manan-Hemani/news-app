74997f7dfed8c25cb945f14cb8977a1d842f0342
Manan <mananhemani1000@gmail.com>
2023-02-15 12:39:29 +0530

    created News.js and NewsItems.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import Navbar from "./Components/Navbar";
+import News from "./Components/News";
+import NewsItems from "./Components/NewsItems";
 
 export default class App extends Component{
   render(){
